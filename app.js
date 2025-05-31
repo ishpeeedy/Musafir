890e703d586132269875ded27c3c756718b9051f
ishpeeedy <anudgesharma@gmail.com>
2025-05-31 15:39:09 +0530

    models basics setup

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const path = require('path')
+const mongoose = require('mongoose')
 const express = require ('express')
+const Campground = require('./models/campground.js')
+
+mongoose.connect('mongodb://localhost:27017/musafir')
+
+const db = mongoose.connection
+db.on("error" , console.error.bind(console,"connection error"))
+db.once("open" , () =>{
+    console.log('db connected')
+})
+
 const app = express()
 
 app.set('view engine','ejs')
@@ -7,6 +18,16 @@ app.set('views',path.join(__dirname , 'views'))
 
 app.get('/' , (req, res)=>{
     res.render("home")
+})
+
+app.get('/makecampground', async (req, res)=>{
+    const camp = new Campground({
+        title:'my backyard',
+        price:'free'
+    })
+    await camp.save()
+    console.log('camp saved')
+    res.send(camp)
 })
 
 app.listen(3000, () =>{
