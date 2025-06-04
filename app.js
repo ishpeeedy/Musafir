9134ac57dc379550151d81ff84113830bed74732
ishpeeedy <anudgesharma@gmail.com>
2025-06-04 18:56:36 +0530

    added a boilerplate

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const mongoose = require('mongoose')
 const express = require ('express')
+const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override')
 const Campground = require('./models/campground.js')
 
