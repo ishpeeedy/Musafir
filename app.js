682d75f7041693e43007f9d743bf4e8058c7d422
ishpeeedy <anudgesharma@gmail.com>
2025-10-17 21:17:39 +0530

    revamped homepage UI

--- app.js
+++ app.js
@@ -1,164 +1,172 @@
-if(process.env.NODE_ENV != "production"){
-    require('dotenv').config()
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
 }
 
-const path = require('path')
-const mongoose = require('mongoose')
-const express = require ('express')
-const ejsMate = require('ejs-mate')
-const session = require('express-session')
-const flash = require('connect-flash')
-const ExpressError = require('./utils/ExpressError')
-const methodOverride = require('method-override')
-const passport = require('passport')
-const LocalStrategy = require('passport-local')
-const User = require('./models/user')
-const helmet  = require('helmet')
+const path = require("path");
+const mongoose = require("mongoose");
+const express = require("express");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const Campground = require("./models/campground");
+const helmet = require("helmet");
 
+const mongoSanitize = require("express-mongo-sanitize");
 
-const mongoSanitize = require('express-mongo-sanitize')
-
-const userRoutes = require('./routes/users')
-const campgroundRoutes  = require('./routes/campgrounds')
-const reviewRoutes= require('./routes/reviews')
-const dbUrl = process.env.DB_URL 
-const MongoDBStore = require('connect-mongo')(session);
+const userRoutes = require("./routes/users");
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
+const dbUrl = process.env.DB_URL;
+const MongoDBStore = require("connect-mongo")(session);
 
 // mongodb://localhost:27017/musafir
 
-mongoose.connect(dbUrl)
+mongoose.connect(dbUrl);
 //     ,{
-//     userNewUrlParser :true , 
+//     userNewUrlParser :true ,
 //     useCreateIndex: true,
 //     useUnifiedTopology : true,
 //     useFindAndModify:true
 // })
 
-const db = mongoose.connection
-db.on("error" , console.error.bind(console,"connection error"))
-db.once("open" , () =>{
-    console.log('db connected')
-})
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("db connected");
+});
 
-const app = express()
+const app = express();
 
-app.engine('ejs',ejsMate)
-app.set('view engine','ejs')
-app.set('views',path.join(__dirname , 'views'))
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded ({extended:true}))
-app.use(methodOverride('_method'))
-app.use(express.static(path.join(__dirname,'public')))
-app.use(mongoSanitize())
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize());
 
 const store = new MongoDBStore({
-    url: dbUrl,
-    secret: 'secrets',
-    touchAfter: 24 * 60 * 60
+  url: dbUrl,
+  secret: "secrets",
+  touchAfter: 24 * 60 * 60,
 });
 
 store.on("error", function (e) {
-    console.log("SESSION STORE ERROR", e)
+  console.log("SESSION STORE ERROR", e);
 });
 
 const sessionConfig = {
-    store,
-    secret : 'secrets',
-    resave : false ,
-    saveUninitialized : true,
-    cookie:{
-        expires : Date.now() +1000*60*60*24*7,
-        maxAge: 1000*60*60*24*7
-    }
-}
-app.use(session(sessionConfig))
-app.use(flash())
+  store,
+  secret: "secrets",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
+app.use(flash());
 app.use(helmet());
 
-
 const scriptSrcUrls = [
-    "https://stackpath.bootstrapcdn.com/",
+  "https://stackpath.bootstrapcdn.com/",
 
-    "https://kit.fontawesome.com/",
-    "https://cdnjs.cloudflare.com/",
-    "https://cdn.jsdelivr.net",
-    "https://cdn.maptiler.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
+  "https://cdn.maptiler.com/",
 ];
 const styleSrcUrls = [
-    "https://kit-free.fontawesome.com/",
-    "https://stackpath.bootstrapcdn.com/",
-    "https://fonts.googleapis.com/",
-    "https://use.fontawesome.com/",
-    "https://cdn.jsdelivr.net",
-    "https://cdn.maptiler.com/", 
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net",
+  "https://cdn.maptiler.com/",
 ];
-const connectSrcUrls = [
-    "https://api.maptiler.com/",
-];
+const connectSrcUrls = ["https://api.maptiler.com/"];
 const fontSrcUrls = [];
 app.use(
-    helmet.contentSecurityPolicy({
-        directives: {
-            defaultSrc: [],
-            connectSrc: ["'self'", ...connectSrcUrls],
-            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-            workerSrc: ["'self'", "blob:"],
-            objectSrc: [],
-            imgSrc: [
-                "'self'",
-                "blob:",
-                "data:",
-                "https://res.cloudinary.com/dzwjyg2ai/",
-                "https://images.unsplash.com/",
-                "https://api.maptiler.com/",
-            ],
-            fontSrc: ["'self'", ...fontSrcUrls],
-        },
-    })
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", "blob:"],
+      objectSrc: [],
+      imgSrc: [
+        "'self'",
+        "blob:",
+        "data:",
+        "https://res.cloudinary.com/dzwjyg2ai/",
+        "https://images.unsplash.com/",
+        "https://api.maptiler.com/",
+      ],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  })
 );
 
-app.use(passport.initialize())
-app.use(passport.session())
-passport.use(new LocalStrategy(User.authenticate()))
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next)=>{
-    res.locals.currentUser = req.user
-    res.locals.success = req.flash('success')
-    res.locals.error = req.flash('error')
-    next()
-})
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
-app.get('/fakeuser',async(req,res)=>{
-    const user= new User ({
-        email:'colt@gmail.com',
-        username:'colttt'
-    })
-    const newUser =await User.register(user,'chicken')
-    res.send(newUser)
-})
+app.get("/fakeuser", async (req, res) => {
+  const user = new User({
+    email: "colt@gmail.com",
+    username: "colttt",
+  });
+  const newUser = await User.register(user, "chicken");
+  res.send(newUser);
+});
 
-app.use('/', userRoutes)
-app.use('/campgrounds', campgroundRoutes)
-app.use('/campgrounds/:id/reviews', reviewRoutes)
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
-app.get('/' , (req, res)=>{
-    res.render("home")
-})
+app.get("/", async (req, res) => {
+  // Fetch specific campgrounds by their IDs
+  const featuredCampgroundIds = [
+    "6905e87d5bc1106b3ad314c2", // Hanumantiya Backwater Island Camp
+    "6905e87d5bc1106b3ad314be", // Maheshwar Fort View Luxury Camp
+    "6905e87d5bc1106b3ad314a5", // Mandu Fort Heritage Camping
+  ];
 
-app.all(/(.*)/, (req, res, next)=> {
-    next(new ExpressError('paige not found ',404))
-})
+  const featuredCampgrounds = await Campground.find({
+    _id: { $in: featuredCampgroundIds },
+  }).limit(3);
 
-app.use((err, req, res , next)=>{
-    const {statusCode =500 , message = "something went wrong"} = err;
-    if(!err.message) err.message = 'oh now! something went wrong'
-    res.status(statusCode).render('error', {err})
-})
+  res.render("home", { featuredCampgrounds });
+});
 
-app.listen(3000, () =>{
-    console.log('serving on post 3000')
-})
+app.all(/(.*)/, (req, res, next) => {
+  next(new ExpressError("paige not found ", 404));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = "something went wrong" } = err;
+  if (!err.message) err.message = "oh now! something went wrong";
+  res.status(statusCode).render("error", { err });
+});
+
+app.listen(3000, () => {
+  console.log("serving on post 3000");
+});
