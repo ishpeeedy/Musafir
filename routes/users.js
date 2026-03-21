f30cd434ae7fe8438beb3f0b703e138c3c3cea70
ishpeeedy <anudgesharma@gmail.com>
2026-03-21 14:07:45 +0530

    Change GET /logout to POST /logout

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,28 @@
-const express = require('express')
-const router = express.Router()
-const passport = require('passport')
-const {storeReturnTo} = require ('../middleware')
-const catchAsync = require('../utils/catchAsync')
-const User = require('../models/user')
-const users= require('../controllers/users')
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const { storeReturnTo } = require("../middleware");
+const catchAsync = require("../utils/catchAsync");
+const User = require("../models/user");
+const users = require("../controllers/users");
 
-router.route('/register')
-    .get(users.renderRegister)
-    .post(catchAsync(users.register))
+router
+  .route("/register")
+  .get(users.renderRegister)
+  .post(catchAsync(users.register));
 
-router.route('/login')
-    .get(users.renderLogin)
-    .post(storeReturnTo ,passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}), users.login)
+router
+  .route("/login")
+  .get(users.renderLogin)
+  .post(
+    storeReturnTo,
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+    }),
+    users.login,
+  );
 
-router.route('/logout')
-    .get(users.logout)
+router.route("/logout").post(users.logout);
 
-module.exports = router
+module.exports = router;
