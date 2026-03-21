6472d3afd6cf2e88a1954940b6c0aa9cf0084786
ishpeeedy <anudgesharma@gmail.com>
2026-03-21 14:04:16 +0530

    Add null check in review creation

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,21 +1,25 @@
-const Campground = require('../models/campground.js')
-const Review = require('../models/review.js')
+const Campground = require("../models/campground.js");
+const Review = require("../models/review.js");
 
-module.exports.createReview = async(req,res)=>{
-    const campground = await Campground.findById(req.params.id)
-    const review = new Review(req.body.review)
-    review.author=req.user._id
-    campground.reviews.push(review)
-    await review.save()
-    await campground.save()
-    req.flash('success','new review created')
-    res.redirect(`/campgrounds/${campground._id}`)
-}
+module.exports.createReview = async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  if (!campground) {
+    req.flash("error", "Campground not found");
+    return res.redirect("/campgrounds");
+  }
+  const review = new Review(req.body.review);
+  review.author = req.user._id;
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  req.flash("success", "new review created");
+  res.redirect(`/campgrounds/${campground._id}`);
+};
 
-module.exports.deleteReview =  async (req,res)=>{
-    const {id , reviewId} = req.params
-    await Campground.findByIdAndUpdate(id,{$pull: {reviews:reviewId}})
-    await Review.findByIdAndDelete(reviewId)
-    req.flash('success','successfully deleted review')
-    res.redirect(`/campgrounds/${id}`)
-}
+module.exports.deleteReview = async (req, res) => {
+  const { id, reviewId } = req.params;
+  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  await Review.findByIdAndDelete(reviewId);
+  req.flash("success", "successfully deleted review");
+  res.redirect(`/campgrounds/${id}`);
+};
